e8203c73f5cf6a01c6155581edbf0a4334e1d9d8
Akshay Nair <phenax5@gmail.com>
2020-10-04 15:10:05 +0530

    Adds domains directory validation and prettified error messages

--- tests/domains.test.js
+++ tests/domains.test.js
@@ -2,15 +2,21 @@ const R = require('ramda');
 const { getDomains, validateDomainData } = require('../utils/domain');
 
 describe('Domains', () => {
-  it('should be valid', async () => {
-    const list = await getDomains();
-
-    list.forEach(data => {
-      const { errors } = validateDomainData(data);
-      if (errors.length) {
-        console.log(errors);
-      }
-    });
+  it('should be valid', (done) => {
+    getDomains()
+      .then(R.map(data => {
+        const { errors } = validateDomainData(data);
+        if (errors.length) {
+          const message = errors
+            .map(([key, { reason }]) => `[${data.name}.${key}]: ${reason}`)
+            .join('\n');
+          return `\nValidation errors in ${data.name}.json: \n${message}`;
+        }
+        return '';
+      }))
+      .then(R.filter(R.complement(R.isEmpty)))
+      .then(messages => messages.length ? done(messages.join('\n')) : done())
+      .catch(done);
   });
 });
 
