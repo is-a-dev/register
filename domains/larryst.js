89413891f5a2dd5390a21ef58bf8295a883bc3ec
Korn <79989518+kornls@users.noreply.github.com>
2022-10-06 11:52:16 +0700

    Registered domain for LARRY STUDIOS™ company

---
+++ domains/larryst.js
@@ -0,0 +1,11 @@
+{
+  "description": "larrystudios.is-a.dev",
+  "repo": "https://github.com/larrystudios/larrystudios.github.io",
+  "owner": {
+    "username": "kornls",
+    "email": "larryst.korn@gmail.com"
+  },
+  "record": {
+    "CNAME": "larrystudios.github.io"
+  }
+}
