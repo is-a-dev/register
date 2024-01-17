317e22394c82678a99026be590c65d91f92564ba
William Harrison <william@williamdavidharrison.com.au>
2024-01-17 11:38:01 +0800

    fix(validation): no ports for A records

--- tests/validations.test.js
+++ tests/validations.test.js
@@ -65,6 +65,7 @@ describe('validateDomainData', () => {
     { ...defaultDomain, record: { A: '::1' } },
     { ...defaultDomain, name: '_discord' },
     { ...defaultDomain, name: '_gitlab-pages-verification-code' },
+    { ...defaultDomain, record: { A: ["123.123.123.123:3000"] } },
   ];
 
   const validCases = [
