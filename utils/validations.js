188715a1623cb397ffb555af8e65db96b4fcbf1b
William Harrison <william@williamdavidharrison.com.au>
2024-01-21 15:52:14 +0800

    feat: acme challenge support

--- utils/validations.js
+++ utils/validations.js
@@ -40,6 +40,7 @@ const extraSupportedNames = [
   testRegex(/^_github(-pages)?-challenge-[a-z0-9-_]+$/i), // Exception for github verification records
   R.equals('_discord'),
   R.equals('_gitlab-pages-verification-code'),
+  R.equals('_acme-challenge'),
 ]
 
 const validateDomainData = validate({
