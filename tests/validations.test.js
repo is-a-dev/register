3f83160b5b8d361e8a883a22dfd6feeb6198c0c1
William Harrison <william@williamdavidharrison.com.au>
2024-01-27 08:25:21 +0800

    no root

--- tests/validations.test.js
+++ tests/validations.test.js
@@ -66,6 +66,7 @@ describe('validateDomainData', () => {
     { ...defaultDomain, name: '_discord' },
     { ...defaultDomain, name: '_gitlab-pages-verification-code' },
     { ...defaultDomain, name: '_acme-challenge' },
+    { ...defaultDomain, name: '_dmarc' },
   ];
 
   const validCases = [
@@ -98,8 +99,8 @@ describe('validateDomainData', () => {
     { ...defaultDomain, name: '_discord.subdomain' },
     { ...defaultDomain, name: '_gitlab-pages-verification-code.subdomain' },
     { ...defaultDomain, name: '_acme-challenge.subdomain' },
-    { ...defaultDomain, name: '_dmarc' },
-    { ...defaultDomain, name: 'dkim._domainkey' },
+    { ...defaultDomain, name: '_dmarc.subdomain' },
+    // { ...defaultDomain, name: 'dkim._domainkey' },
   ];
 
   it('should return false for invalid data', () => {
