28f684055bdf1d50e6bdc50bdb9065ffd71955b7
Okan Akdemir <88800684+okan12300@users.noreply.github.com>
2024-07-14 15:57:05 +0300

    Create hostname.js

---
+++ domains/hostname.js
@@ -0,0 +1,9 @@
+{
+    "owner": {
+        "username": "_github-pages-challenge-okan12300.okan12300",
+        "email": "okanakd89@outlook.com"
+    },
+    "record": {
+        "TXT": "99f230ddd368e4b43322de37eb162f"
+    }
+}
