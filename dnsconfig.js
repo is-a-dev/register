20ad787b7d35a015d53491bf4553c73b3dc687ea
andrewstech <andrew@m.is-a.dev>
2024-09-25 03:02:08 +0100

    DNS control config

---
+++ dnsconfig.js
@@ -0,0 +1,88 @@
+var regNone = NewRegistrar("none");
+var providerPdns = DnsProvider(NewDnsProvider("powerdns"));
+
+function getDomainsList(filesPath) {
+  var result = [];
+  var files = glob.apply(null, [filesPath, true, '.json']);
+
+  for (var i = 0; i < files.length; i++) {
+    var basename = files[i].split('/').reverse()[0];
+    var name = basename.split('.')[0];
+
+    result.push({ name: name, data: require(files[i]) });
+  }
+
+  return result;
+}
+
+var domains = getDomainsList('./domains');
+var commit = {};
+
+for (var idx in domains) {
+  var domainData = domains[idx].data;
+
+  if (!commit[domains[idx].name]) {
+    commit[domains[idx].name] = [];
+  }
+
+  // Handle A records
+  if (domainData.record.A) {
+    for (var a in domainData.record.A) {
+      commit[domains[idx].name].push(
+        A('', IP(domainData.record.A[a])) // Assuming root domain for 'subdomain'
+      );
+    }
+  }
+
+  // Handle AAAA records
+  if (domainData.record.AAAA) {
+    for (var aaaa in domainData.record.AAAA) {
+      commit[domains[idx].name].push(
+        AAAA('', domainData.record.AAAA[aaaa])
+      );
+    }
+  }
+
+  // Handle CNAME records
+  if (domainData.record.CNAME) {
+    commit[domains[idx].name].push(
+      CNAME('', domainData.record.CNAME + ".")
+    );
+  }
+
+  // Handle URL records (for redirects)
+  if (domainData.record.URL) {
+    commit[domains[idx].name].push(
+      URL('', domainData.record.URL)
+    );
+  }
+
+  // Handle MX records
+  if (domainData.record.MX) {
+    for (var mx in domainData.record.MX) {
+      commit[domains[idx].name].push(
+        MX('', 10, domainData.record.MX[mx] + ".") // Default priority is set to 10
+      );
+    }
+  }
+
+  // Handle TXT records
+  if (domainData.record.TXT) {
+    if (Array.isArray(domainData.record.TXT)) {
+      for (var txt in domainData.record.TXT) {
+        commit[domains[idx].name].push(
+          TXT('', domainData.record.TXT[txt])
+        );
+      }
+    } else {
+      commit[domains[idx].name].push(
+        TXT('', domainData.record.TXT)
+      );
+    }
+  }
+}
+
+// Commit all DNS records
+for (var domainName in commit) {
+  D(domainName, regNone, providerPdns, commit[domainName]);
+}
