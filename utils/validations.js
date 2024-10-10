1fe690c651090ee9eea91318b57db3fcd3b54f5d
Archit Kumar <97726243+saxophone-guy@users.noreply.github.com>
2024-10-10 08:05:50 +0530

    Update validations.js

--- utils/validations.js
+++ utils/validations.js
@@ -61,7 +61,6 @@ const extraSupportedNames = [
     R.equals("_domainkey"),
     R.equals("_improvmx"),
     R.equals("_vercel"),
-    R.equals("_dnslink"),
     testRegex(/^_gh-[a-z0-9-_]+$/i),
 ];
 
