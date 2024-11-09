f33e2f4f469b3dd69e4a713796936f51f55fb1bf
William Harrison <87287585+wdhdev@users.noreply.github.com>
2024-11-09 20:43:57 +0800

    Update rename-to-lowercase.js

--- scripts/rename-to-lowercase.js
+++ scripts/rename-to-lowercase.js
@@ -3,21 +3,21 @@ const path = require("path");
 
 const directoryPath = path.join(__dirname, "../domains");
 
-(async () => {
+function renameFilesToLowercase() {
     // Read the files in the 'domains' directory
-    fs.readdirSync(directoryPath, (err, files) => {
+    fs.readdir(directoryPath, (err, files) => {
         if (err) {
             console.error("Error reading directory:", err);
             return;
         }
 
-        files.forEach(async (file) => {
+        files.forEach((file) => {
             const oldPath = path.join(directoryPath, file);
             const newPath = path.join(directoryPath, file.toLowerCase());
 
             // Only rename if the file name is not already in lowercase
             if (oldPath !== newPath) {
-                fs.renameSync(oldPath, newPath, (err) => {
+                fs.rename(oldPath, newPath, (err) => {
                     if (err) {
                         console.error("Error renaming file:", err);
                     } else {
@@ -27,4 +27,6 @@ const directoryPath = path.join(__dirname, "../domains");
             }
         });
     });
-})();
+}
+
+renameFilesToLowercase();
