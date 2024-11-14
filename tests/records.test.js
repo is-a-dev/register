43fae0da64af4573495be3cb40161382860de947
William Harrison <87287585+wdhdev@users.noreply.github.com>
2024-11-14 13:40:24 +1100

    dont allow root to cname itself

--- tests/records.test.js
+++ tests/records.test.js
@@ -204,10 +204,18 @@ t("All files should have valid record values", (t) => {
 
                 if (key === "CNAME") {
                     t.regex(value, hostnameRegex, `${file}: Record value should be a valid hostname for ${key}`);
-                    t.false(
-                        value === file.replace(/\.json$/, "") + ".is-a.dev",
-                        `${file}: Record value should not reference itself for ${key}`
-                    );
+
+                    if(file === "@.json") {
+                        t.false(
+                            value === "is-a.dev",
+                            `${file}: Record value should not reference itself for ${key}`
+                        );
+                    } else {
+                        t.false(
+                            value === file.replace(/\.json$/, "") + ".is-a.dev",
+                            `${file}: Record value should not reference itself for ${key}`
+                        );
+                    }
                 }
 
                 if (key === "URL") {
