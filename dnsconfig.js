0b55ae45caed26a12db81dad8c4ca5f0bf65345f
William Harrison <87287585+wdhdev@users.noreply.github.com>
2024-11-27 18:58:48 +0800

    test custom ignored

--- dnsconfig.js
+++ dnsconfig.js
@@ -162,6 +162,12 @@ var ignored = [
     IGNORE("dkim._domainkey", "TXT")
 ];
 
+const ignoredDomains = require("./utils/ignored-domains.json");
+
+for (var subdomain in ignoredDomains) {
+    ignored.push(IGNORE(subdomain, ignoredDomains[subdomain]));
+}
+
 // Push TXT record of when the zone was last updated
 records.push(TXT("_zone-updated", "\"" + Date.now().toString() + "\""));
 
