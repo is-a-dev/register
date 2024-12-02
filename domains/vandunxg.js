12ee82b5acb2e886acf272e77c469be9f009c833
Nguyen Van Dung <64525493+vandunxg@users.noreply.github.com>
2024-12-02 16:31:44 +0700

    Create vandunxg.js

---
+++ domains/vandunxg.js
@@ -0,0 +1,10 @@
+{
+  "description": "This subdomain is for my personal portfolio website.",
+  "owner": {
+    "username": "vandunxg",
+    "email": "nguyenvandung.official@gmail.com",
+  },
+  "record": {
+    "NS": ["adel.ns.cloudflare.com", "carl.ns.cloudflare.com"]
+  }
+}
