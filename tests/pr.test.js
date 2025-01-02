ec4e7b5676454c61ac53c2ea51bafea99cc4be16
William Harrison <87287585+wdhdev@users.noreply.github.com>
2025-01-02 19:53:15 +0800

    maybe this will fix it

--- tests/pr.test.js
+++ tests/pr.test.js
@@ -3,11 +3,7 @@ const fs = require("fs-extra");
 const path = require("path");
 
 const PR_AUTHOR = process.env.PR_AUTHOR;
-let MODIFIED_FILES = process.env.MODIFIED_FILES.split(" ");
-
-for (let i = 0; i < MODIFIED_FILES.length; i++) {
-    MODIFIED_FILES[i] = MODIFIED_FILES[i].substring(MODIFIED_FILES[i].lastIndexOf("/") + 1);
-}
+const MODIFIED_FILES = process.env.MODIFIED_FILES.split(" ");
 
 const domainsPath = path.resolve("domains");
 const headDomainsPath = path.resolve(`register-${process.env.RUN_ID}/domains`);
@@ -21,11 +17,11 @@ t("Modified JSON files must be owned by the PR author", (t) => {
     }
 
     MODIFIED_FILES.forEach((file) => {
-        const modifiedDomain = fs.readJsonSync(path.join(domainsPath, file));
+        const modifiedDomain = fs.readJsonSync(path.join(domainsPath, file.substring(MODIFIED_FILES[i].lastIndexOf("/") + 1)));
         let currentDomain = null;
 
         try {
-            currentDomain = fs.readJsonSync(path.join(headDomainsPath, file));
+            currentDomain = fs.readJsonSync(path.join(headDomainsPath, file.substring(MODIFIED_FILES[i].lastIndexOf("/") + 1)));
         } catch {
             currentDomain = modifiedDomain;
         }
@@ -44,10 +40,10 @@ t("New JSON files must be owned by the PR author", (t) => {
         return;
     }
 
-    const newFiles = fs.readdirSync(domainsPath).filter((file) => !fs.readdirSync(headDomainsPath).includes(file));
+    const newFiles = fs.readdirSync(domainsPath).filter((file) => !fs.readdirSync(headDomainsPath).includes(file.substring(MODIFIED_FILES[i].lastIndexOf("/") + 1)));
 
     newFiles.forEach((file) => {
-        const domain = fs.readJsonSync(path.join(domainsPath, file));
+        const domain = fs.readJsonSync(path.join(domainsPath, file.substring(MODIFIED_FILES[i].lastIndexOf("/") + 1)));
 
         t.true(
             domain.owner.username === PR_AUTHOR || admins.includes(PR_AUTHOR),
