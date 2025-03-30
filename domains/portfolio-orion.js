87cd7f080fa222d6b72012a4d5ab8309fba294c4
IamOrion <105987929+Orionwilx@users.noreply.github.com>
2025-03-30 14:44:59 -0500

    Create portfolio-orion.js
    Create subdominio for personal porfolio

---
+++ domains/portfolio-orion.js
@@ -0,0 +1,11 @@
+{
+    "description": "Portfolio para mostrar habilidades ganadas",
+    "repo": "https://github.com/Orionwilx/personal-portfolio",
+    "owner": {
+        "username": "Orionwilx",
+        "email": "wilman-3d@hotmail.com"
+    },
+    "record": {
+        "A": ["75.2.60.5"]
+    }
+}
