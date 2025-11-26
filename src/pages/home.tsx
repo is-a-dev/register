bfcedc1fa2690d7a52673e399e2d7aa35823c731
Fuis18 <96286214+Fuis18@users.noreply.github.com>
2025-11-25 22:39:27 -0500

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,7 +5,20 @@ import Main from "@/components/Main";
 export default function Home() {
 	return (
 		<Main title="Todos los Proyectos" className="cont__flex">
-			<p>Esta es la página de inicio de tu aplicación.</p>
+			<p>Esta es mi aplicación personal la iré actualizando y subiendo mis proyectos a medida que avance.</p>
+			<p>Pienso tener 3 secciones</p>
+			<ul>
+				<li>una de presentación donde habrán insignias de los lenguajes que sé</li>
+				<li>Otra sobre los proyectos</li>
+				<li>Otra sobre contacto</li>
+			</ul>
+			<p>Lamento si no parece completo, ando refactorizando desde mi página anterior <a href="fuis18.github.com">Old Page</a> y <a href="fuis18-next.vercel.com">old Page</a></p>
+			<p>Estado metido últimamente en hacer mi dotfiles para Archlinux, visualmente ya lo acabe, falta Nvchad y que se conecte a mis audifonos cuando lo conecto por bluetooth</p>
+			<p>Comprendo si hay dificultades si hablas ingles, también podre un intl, solo que la universidad y el trabajo no me dejan</p>
+			<p>Aunque no es un trabajo de la industria, es de mesero, tengo compañeros muy bueno, pero la pasión de la programación nunca la dejaré</p>
+			<p>Otra página que hice para una amiga que si la complete, jeje <a href="healthy-life-a.vercel.app/">Healhy</a>, comprendo que parece de comercio, pero solo le ayude para una feria para la universidad, no hay ninguna empresa en sí</p>
+			<p>Si me lo permiten, le pondré mucho esfuerzo a esta página :D</p>
 		</Main>
 	);
 }
+
